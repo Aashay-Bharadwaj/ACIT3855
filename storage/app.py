b53afc16263f0c522c3ad22ea46f9f30dfbd2e90
Aashay-Bharadwaj <98194521+Aashay-Bharadwaj@users.noreply.github.com>
2023-11-16 10:24:56 -0800

    Update app.py

--- storage/app.py
+++ storage/app.py
@@ -128,45 +128,45 @@ def get_standard_order(timestamp):
     return results_list, 200
 
 
-def process_messages():
-    """ Process event messages """
-    hostname = "%s:%d" % (app_config["events"]["hostname"],
-                          app_config["events"]["port"])
-    client = KafkaClient(hosts=hostname)
-    topic = client.topics[str.encode(app_config["events"]["topic"])]
+# def process_messages():
+#     """ Process event messages """
+#     hostname = "%s:%d" % (app_config["events"]["hostname"],
+#                           app_config["events"]["port"])
+#     client = KafkaClient(hosts=hostname)
+#     topic = client.topics[str.encode(app_config["events"]["topic"])]
 
-    # Create a consume on a consumer group, that only reads new messages
-    # (uncommitted messages) when the service re-starts (i.e., it doesn't
-    # read all the old messages from the history in the message queue).
-    consumer = topic.get_simple_consumer(consumer_group=b'event_group',
-                                         reset_offset_on_start=False,
-                                         auto_offset_reset=OffsetType.LATEST)
+#     # Create a consume on a consumer group, that only reads new messages
+#     # (uncommitted messages) when the service re-starts (i.e., it doesn't
+#     # read all the old messages from the history in the message queue).
+#     consumer = topic.get_simple_consumer(consumer_group=b'event_group',
+#                                          reset_offset_on_start=False,
+#                                          auto_offset_reset=OffsetType.LATEST)
 
-    # This is blocking - it will wait for a new message
-    for msg in consumer:
-        msg_str = msg.value.decode('utf-8')
-        msg = json.loads(msg_str)
-        logger.info("Message: %s" % msg)
+#     # This is blocking - it will wait for a new message
+#     for msg in consumer:
+#         msg_str = msg.value.decode('utf-8')
+#         msg = json.loads(msg_str)
+#         logger.info("Message: %s" % msg)
 
-        payload = msg["payload"]
+#         payload = msg["payload"]
 
-        if msg["type"] == "item":  # Change this to your event type
-            # Store the event1 (i.e., the payload) to the DB
-            report_inventory_item(payload)
-        elif msg["type"] == "order":  # Change this to your event type
-            # Store the event2 (i.e., the payload) to the DB
-            report_standard_order(payload)
+#         if msg["type"] == "item":  # Change this to your event type
+#             # Store the event1 (i.e., the payload) to the DB
+#             report_inventory_item(payload)
+#         elif msg["type"] == "order":  # Change this to your event type
+#             # Store the event2 (i.e., the payload) to the DB
+#             report_standard_order(payload)
 
-        # Commit the new message as being read
-        consumer.commit_offsets()
+#         # Commit the new message as being read
+#         consumer.commit_offsets()
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
-    t1 = Thread(target=process_messages)
-    t1.setDaemon(True)
-    t1.start()
+    # t1 = Thread(target=process_messages)
+    # t1.setDaemon(True)
+    # t1.start()
 
     app.run(port=8090)
