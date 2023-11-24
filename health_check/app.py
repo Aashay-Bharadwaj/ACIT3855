e6c7463f49dcca99d4ecd87ee309cc6a25cd0a70
Aashay-Bharadwaj <98194521+Aashay-Bharadwaj@users.noreply.github.com>
2023-11-23 21:13:15 -0800

    test

--- health_check/app.py
+++ health_check/app.py
@@ -3,12 +3,11 @@ import datetime
 import requests
 import json
 import yaml
-import logging, logging.config
+import logging
 from apscheduler.schedulers.background import BackgroundScheduler
+from flask import Response
 
-
-
-
+# Load configurations
 with open('app_conf.yml', 'r') as f:
     app_config = yaml.safe_load(f.read())
 
@@ -24,55 +23,62 @@ def get_status():
     try:
         with open(app_config['datastore']['filename'], 'r') as file:
             status = json.load(file)
-    except:
+    except FileNotFoundError:
         return Response("Status records do not exist", 404)
     
-    obj = {"receiver": status['receiver'], "storage": status["storage"],
-           "audit": status["audit"], "processing": status["processing"], "last_updated": status['last_updated']}
+    obj = {
+        "receiver": status.get('receiver', 'unknown'),
+        "storage": status.get("storage", 'unknown'),
+        "audit": status.get("audit", 'unknown'),
+        "processing": status.get("processing", 'unknown'),
+        "last_updated": status.get('last_updated', 'unknown')
+    }
     
     logger.debug(f'{obj}')
 
     logger.info("Request has completed.")
 
-    return Response(json.dumps(obj), 200)
-    
-
+    return Response(json.dumps(obj), status=200, mimetype='application/json')
 
 def populate_status():
     logger.info("Starting service check process")
 
-    with open(app_config['datastore']['filename'], 'r') as file:
-        stats = json.load(file)
-    
-    time = stats['last_updated']
+    try:
+        with open(app_config['datastore']['filename'], 'r') as file:
+            stats = json.load(file)
+    except FileNotFoundError:
+        logger.error("Datastore file not found.")
+        return
+
+    time = stats.get('last_updated', datetime.datetime.now())
     print(time)
-    next_time = datetime.datetime.now()
+
     start_time = time.time()
     audit = requests.get(app_config["eventstore"]["url"] + "/health")
     end_time = time.time()
 
     elapsed_time = end_time - start_time
+
     if elapsed_time > 5:
         stats['audit'] = "down"
-        print(f"Request took {elapsed_time} seconds, which is more than 5 seconds.")
+        logger.warning(f"Request took {elapsed_time} seconds, which is more than 5 seconds.")
     else:
         stats['audit'] = "running"
-        print(f"Request took {elapsed_time} seconds.")
-     
-    # order = requests.get(app_config["eventstore"]["url"] + "/standard-order?timestamp=" + '2013-11-23T08:30:00Z')
+        logger.info(f"Request took {elapsed_time} seconds.")
+
     print(stats)
+
 def get_health():
-    return 200
+    return "OK", 200
 
 def init_scheduler():
     sched = BackgroundScheduler(daemon=True)
-    sched.add_job(populate_status,
-                'interval',
-                seconds=app_config['scheduler']['period_sec'])
+    sched.add_job(populate_status, 'interval', seconds=app_config['scheduler']['period_sec'])
     sched.start()
+
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
 
 if __name__ == '__main__':
     init_scheduler()
-    app.run(port='8120')
+    app.run(port=8120)
