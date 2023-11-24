13c273c25598d3898c814374dd6133149275e64d
Aashay-Bharadwaj <98194521+Aashay-Bharadwaj@users.noreply.github.com>
2023-11-23 22:17:31 -0800

    Update app.py

--- health_check/app.py
+++ health_check/app.py
@@ -59,7 +59,7 @@ def populate_status():
     processing = requests.get(app_config["eventstore"]["url_processing"] + "/health", timeout=5)
     receiver = requests.get(app_config["eventstore"]["url_receiver"] + "/health", timeout=5)
     print(audit)
-    if audit == 200:
+    if 200 in audit:
         stats['audit'] = "down"
         
     else:
