9f311cf1c7ea0480df0ab1e0bd1bba15c8ccbae9
Aashay-Bharadwaj <98194521+Aashay-Bharadwaj@users.noreply.github.com>
2023-11-23 20:58:35 -0800

    A2

---
+++ health_check/app.py
@@ -0,0 +1,82 @@
+import connexion
+import datetime
+import requests
+import json
+from flask import Response
+from flask_cors import CORS, cross_origin
+import yaml
+import logging, logging.config
+import uuid
+from apscheduler.schedulers.background import BackgroundScheduler
+
+app = connexion.FlaskApp(__name__, specification_dir='')
+CORS(app.app)
+app.app.config['CORS_HEADERS'] = 'Content-Type'
+
+with open('app_conf.yml', 'r') as f:
+    app_config = yaml.safe_load(f.read())
+
+with open('log_conf.yml', 'r') as f:
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
+
+logger = logging.getLogger('basicLogger')
+
+def get_status():
+    logger.info("Request for Status check has begun.")
+    
+    try:
+        with open(app_config['datastore']['filename'], 'r') as file:
+            status = json.load(file)
+    except:
+        return Response("Status records do not exist", 404)
+    
+    obj = {"receiver": status['receiver'], "storage": status["storage"],
+           "audit": status["audit"], "processing": status["processing"], "last_updated": status['last_updated']}
+    
+    logger.debug(f'{obj}')
+
+    logger.info("Request has completed.")
+
+    return Response(json.dumps(obj), 200)
+    
+
+
+def populate_status():
+    logger.info("Starting service check process")
+
+    with open(app_config['datastore']['filename'], 'r') as file:
+        stats = json.load(file)
+    
+    time = stats['last_updated']
+    print(time)
+    next_time = datetime.datetime.now()
+    start_time = time.time()
+    audit = requests.get(app_config["eventstore"]["url"] + "/health")
+    end_time = time.time()
+
+    elapsed_time = end_time - start_time
+    if elapsed_time > 5:
+        stats['audit'] = "down"
+        print(f"Request took {elapsed_time} seconds, which is more than 5 seconds.")
+    else:
+        stats['audit'] = "running"
+        print(f"Request took {elapsed_time} seconds.")
+     
+    # order = requests.get(app_config["eventstore"]["url"] + "/standard-order?timestamp=" + '2013-11-23T08:30:00Z')
+    print(stats)
+def get_health():
+    return 200
+
+def init_scheduler():
+    sched = BackgroundScheduler(daemon=True)
+    sched.add_job(populate_status,
+                'interval',
+                seconds=app_config['scheduler']['period_sec'])
+    sched.start()
+
+app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
+
+if __name__ == '__main__':
+    init_scheduler()
+    app.run(port='8100')
